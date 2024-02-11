9496db221e67010f68adab73854e33b584324109
bil <bil@malmaute.us>
2024-02-10 20:50:55 -0500

    added milstein sticker notes

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -8,11 +8,12 @@
 <p><em>~~ digitalsketches</em></p>
 
 <h2>Stickers</h2>
+<p>202311.. <a href="/projects/sticker-milstein-computer">Milstein Retro Computer</a></p>
 <p>20231107 <a href="/projects/hender-code-sticker">Henderson Cat QR Code</a></p>
 <p><em>~~ Babes Brunch</em></p>
 <p><em>~~ Heraldry</em></p>
 <p><em>~~ eat.EXE</em></p>
-<p><em></em></p>
+
 
 <h2>Explorations</h2>
 <p><em>~~ Random Boolean Networks</em></p>
@@ -20,6 +21,11 @@
 
 <h2>Physical</h2>
 <p>20231112 <a href="/projects/catharstick">Catharstick</a></p>
+
+<h2>Other</h2>
+<p><em>~~ cardboard AR wordle clone</em></p>
+<p><em>~~ openRC mixed reality</em></p>
+
 
 
 <style>
---
+++ src/routes/projects/sticker-milstein-computer/+page.svelte
@@ -0,0 +1,55 @@
+<script>
+    import mil1 from "$lib/img/sticker-milstein-computer/stable.jpg"
+    import mil2 from "$lib/img/sticker-milstein-computer/gimp.png"
+    import mil3 from "$lib/img/sticker-milstein-computer/pack.jpg"
+    import mil4 from "$lib/img/sticker-milstein-computer/table.jpg"
+    import mil5 from "$lib/img/sticker-milstein-computer/lap.jpg"
+    import mil6 from "$lib/img/sticker-milstein-computer/tag.jpg"
+    
+</script>
+
+<h1>Milstein Micro Residency Stickers</h1>
+
+<p><em>a simple sticker for the students in the micro-residency pilot.</em></p>
+
+<div class="image-track">
+    <img src={mil1} alt="">
+    <img src={mil2} alt="">
+</div>
+
+<p>We were coming up on the last couple weeks before the Fall Open Studio Event. Since there was no sale at <a href="https://www.stickermule.com/">Sticker Mule</a> I decided to browse available sticker materials. The glitter (as opposed to the holographic) backing made me think about the static on an old CRT screen.</p>
+
+<p>I hopped into stable diffusion and began trying out different prompts for retro computers. I was looking for an Apple II aesthetic. A style that ended up working incorporated "8-bit" and "pixel graphics".</p>
+
+<div class="image-track">
+    <img src={mil3} alt="">
+    <img src={mil4} alt="">
+</div>
+
+<p>The image was worked up in GIMP. Colors were saturated and then run through a pixel filter. The screen portion and the background of the image were removed leaving only transparency where the sticker backing would be visible (i.e. static on the display).</p>
+
+<p>A single letter M was rendered in yellow on the center of the display to mimic the Milstein Program logo.</p>
+
+
+
+<p>50 stickers were ordered for the event at 2 in x 2 in.</p>
+
+<div class="image-track">
+    <img src={mil5} alt="">
+    <img src={mil6} alt="">
+</div>
+
+<style>
+    .image-track {
+        width: 100%;
+        display: flex;
+        flex-wrap: wrap;
+        gap: 0.3rem;
+    }
+    img {
+        flex: 1 1 250px;
+        height: 300px;
+        object-fit:contain;
+    }
+
+</style>
