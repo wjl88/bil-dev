1ac24e461f687f8966735c142064d1fad5b77ea2
bil <bil@malmaute.us>
2024-05-14 23:39:32 -0400

    refactored img tags

--- src/routes/cv/+page.svelte
+++ src/routes/cv/+page.svelte
@@ -1,9 +1,7 @@
 <h1>Education</h1>
 
 <p>Tech MBA, Cornell</p>
-<p>
-    Ph.D. in Chemistry, Lehigh University
-</p>
+<p>Ph.D. in Chemistry, Lehigh University</p>
 <p>BS in Chemistry, West Chester University</p>
 
 <h1>Skills I have no business having:</h1>
@@ -12,11 +10,11 @@
     off-road racing, laser engraving, dungeon mastering, improv everything
 </p>
 <h1>Computer languages I don't hate:</h1>
-<p>igor pro, svelte, C++, C#, python, javascript, bash</p>
+<p>igor pro, svelte, c++, c#, python, javascript, bash</p>
 <h1>Computing that makes me happy:</h1>
 <p>
-    igor pro, emacs, blender, gimp, shotcut, unity3D, pytorch, arduino, audacity, embedded systems, nginx, i3, linux
-    everything
+    igor pro, emacs, blender, gimp, shotcut, unity3D, pytorch, arduino,
+    audacity, embedded systems, nginx, i3, linux everything
 </p>
 <h1>Tiny things</h1>
 <p>tinyphone, falcon, svelte, jetson nano, raspberry pi zero</p>
@@ -24,7 +22,8 @@
 <p>
     Mentor for Girl Scout Robotics team The Tech Hoppers, K-12 Educational
     Outreach (Cornell Tech), Science Fair Judge, Graduate School Application
-    mentor (Lehigh, Cornell), International Student Orientation Leader (Lehigh), Graduate Summer Softball League Commissioner (Lehigh)
+    mentor (Lehigh, Cornell), International Student Orientation Leader (Lehigh),
+    Graduate Summer Softball League Commissioner (Lehigh)
 </p>
 
 <style>
